006f59ac9fd4fbf92f3c0cbc73d89823932e59c3
KOMIYA Atsushi <komiya.atsushi@gmail.com>
2016-06-28 01:52:56 +0900

    Add benchmark: Gaussian

---
+++ benchmark/src/jmh/java/biz/k11i/rng/GaussianBenchmark.java
@@ -0,0 +1,29 @@
+package biz.k11i.rng;
+
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+@State(Scope.Thread)
+public class GaussianBenchmark {
+    private Random javaUtilRandom = new Random();
+
+    @Benchmark
+    public double javaUtilRandom() {
+        return javaUtilRandom.nextGaussian();
+    }
+
+    @Benchmark
+    public double threadLocalRandom() {
+        return ThreadLocalRandom.current().nextGaussian();
+    }
+
+    @Benchmark
+    public double fastRng() {
+        return GaussianRNG.FAST_RNG.generate(ThreadLocalRandom.current());
+    }
+}
